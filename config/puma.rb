feaf97c8356b6d7f3140fee5091b7b49ee97755c
snmmaurya <smspace@gmail.com>
2024-10-17 15:11:32 +0530

    f

--- config/puma.rb
+++ config/puma.rb
@@ -1,33 +1,28 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# config/puma.rb
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# Specifies the minimum and maximum threads Puma will use to serve requests.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests.
+port        ENV.fetch("PORT") { 3000 }
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# Specifies the `environment` that Puma will run in.
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the number of workers to fork (in clustered mode).
+# Workers are forked web server processes. If using threads and workers together,
+# this allows Puma to be multi-threaded while serving more concurrent requests.
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# Preload the application for better performance.
+preload_app!
+
+# On worker boot:
+# - If using threads, reconnect any thread-based services after forking.
+# - Reconnect to the database here.
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+# Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-# Only use a pidfile when requested
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
