bc0c9ab0a1cea0530058b807db69e451c4d0918f
snmmaurya <smspace@gmail.com>
2024-11-08 11:48:35 +0530

    credentials seperated for each environment

--- scripts/configure_master_key.sh
+++ scripts/configure_master_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "START master.key Configuration"
 sudo -u ec2-user -i <<'EOF'
-aws ssm get-parameter --name "/snm/master_key" --with-decryption --query Parameter.Value --output text > /home/ec2-user/snm/config/master.key
+sudo sh -c 'aws ssm get-parameter --name "/snm/master_key" --with-decryption --query Parameter.Value --output text > /home/ec2-user/snm/config/credentials/production.key'
 EOF
 echo "DONE master.key Configuration"
