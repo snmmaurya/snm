34d065395ab73eee0870b13f5978076b1440fea8
snmmaurya <smspace@gmail.com>
2024-11-08 19:53:08 +0530

    chown -R ec2-user:ec2-user /home/ec2-user/snm/tmp

--- scripts/configure_ec2_user_ownership.sh
+++ scripts/configure_ec2_user_ownership.sh
@@ -2,5 +2,6 @@
 echo "START ec2-user Ownership"
 sudo -u ec2-user -i <<'EOF'
 sudo chown -R ec2-user:ec2-user /home/ec2-user/snm
+sudo chown -R ec2-user:ec2-user /home/ec2-user/snm/tmp
 EOF
 echo "Done ec2-user Ownership"
